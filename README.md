907ddb3faa194ab317668915a77ae58595054364
webfaisalbd <web.faisal.bd@gmail.com>
2023-03-18 13:33:34 +0600

    loading added

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ### ADVICE ME REACT APP
 
+website: https://advice-me-react-app.netlify.app/
+
 ##### Using:
  - axios
  - tailwind css
+
+ ---
+
+ <img scr='/public/assets/website.png' />
--- src/components/Advice/Advice.js
+++ src/components/Advice/Advice.js
@@ -5,21 +5,31 @@ import axios from 'axios'
 const Advice = () => {
 
   const [advice, setAdvice] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const adviceFunc = () => {
     axios.get('https://api.adviceslip.com/advice')
       .then(function (res) {
         setAdvice(res.data.slip.advice);
+        setLoading(false);
       })
   }
 
   adviceFunc();
 
   return (
-    <div className='flex flex-col justify-center items-center h-screen bg-wallpaper bg-cover bg-center bg-no-repeat'>
-        <h2 className='text-3xl text-white font-bold font-julee px-6 w-[75%] text-center'>{advice}</h2>
-        <button className='text-center bg-blue-500 hover:bg-red-400 rounded px-8 py-2 mt-6 text-white font-bold text-2xl' onClick={adviceFunc}>Again</button>
-      </div>
+    <>
+      {
+        loading === true ? <div className='h-screen flex items-center justify-center'>
+          <div className='loading'></div>
+        </div> :
+          <div className='flex flex-col justify-center items-center h-screen bg-wallpaper bg-cover bg-center bg-no-repeat'>
+            <h2 className='text-3xl text-white font-bold font-julee px-6 w-[75%] text-center'>{advice}</h2>
+            <button className='text-center bg-blue-500 hover:bg-red-400 rounded px-8 py-2 mt-6 text-white font-bold text-2xl' onClick={adviceFunc}>Again</button>
+          </div>
+      }
+
+    </>
   )
 }
 
